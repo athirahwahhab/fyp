a9fe11bee6b9bca327bf0eb7b2c0bdaf8dd5f9a2
athirahwahhab <93929023+athirahwahhab@users.noreply.github.com>
2025-01-02 10:33:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -185,3 +185,30 @@ plt.grid(True)
 plt.show()
 st.pyplot(plt.gcf())
 
+st.title("Sayur Sawi Hijau")
+# Filter data for item_code = 1558
+item_1558_data = df[df['item_code'] == 1558].copy()
+
+# Convert 'date' to datetime format
+item_1558_data['date'] = pd.to_datetime(item_1558_data['date'], format='%d-%b-%y')
+
+# Set 'date' as the index
+item_1558_data.set_index('date', inplace=True)
+
+# Aggregating prices by date (average price per day)
+item_1558_daily_prices = item_1558_data.groupby('date')['price'].mean()
+
+# Display the processed data
+item_1558_daily_prices.head(), item_1558_daily_prices.info()
+
+st.title("Sayur Sawi Hijau Daily Average Prices ")
+# Plot the time series data
+plt.figure(figsize=(12, 6))
+plt.plot(item_1558_daily_prices, label='Daily Average Price', color='blue')
+plt.title('Daily Average Prices of Item 1558', fontsize=16)
+plt.xlabel('Date', fontsize=12)
+plt.ylabel('Price (RM)', fontsize=12)
+plt.legend()
+plt.grid(True)
+plt.show()
+st.pyplot(plt.gcf())
