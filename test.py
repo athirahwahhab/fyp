f6662029b4b2ecd9805e57475cb39ee61430bc9f
athirahwahhab <93929023+athirahwahhab@users.noreply.github.com>
2025-01-02 10:20:35 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,8 +9,7 @@ file_path = 'https://raw.githubusercontent.com/athirahwahhab/fyp/refs/heads/main
 df = pd.read_csv(file_path)    
 # Title for the Streamlit app
 st.title("Price Trend Analysis")
-    
-    
+st.title("Ayam Standard ")
 item_code = 1  # AYAM STANDARD
 df_item = df[df['item_code'] == item_code]  # Filter data by item code
 
@@ -42,7 +41,7 @@ item_1_daily_prices = item_1_data.groupby('date')['price'].mean()
 # Display the processed data
 item_1_daily_prices.head(), item_1_daily_prices.info()
 
-import matplotlib.pyplot as plt
+st.title("Daily Ayam Standard Average  ")
 
 # Plot the time series data
 plt.figure(figsize=(12, 6))
@@ -55,4 +54,32 @@ plt.grid(True)
 plt.show()
 # Display the plot in Streamlit
 st.pyplot(plt.gcf())
+
+st.title("Ayam Super")
+
+item_2_data = df[df['item_code'] == 2 ].copy()
+
+# Convert 'date' to datetime format
+item_2_data['date'] = pd.to_datetime(item_2_data['date'], format='%d-%b-%y')
+
+# Set 'date' as the index
+item_2_data.set_index('date', inplace=True)
+
+# Aggregating prices by date (average price per day)
+item_2_daily_prices = item_2_data.groupby('date')['price'].mean()
+
+# Display the processed data
+item_2_daily_prices.head(), item_2_daily_prices.info()
+
+st.title("Ayam Super Average Daily Prices ")
+
+# Plot the time series data
+plt.figure(figsize=(12, 6))
+plt.plot(item_2_daily_prices, label='Daily Average Price', color='blue')
+plt.title('Daily Average Prices of Item 2', fontsize=16)
+plt.xlabel('Date', fontsize=12)
+plt.ylabel('Price (RM)', fontsize=12)
+plt.legend()
+plt.grid(True)
+plt.show()
 
