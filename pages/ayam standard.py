68e6fc6d55878fc17f92f74c5a83fadfa31ac49f
athirahwahhab <93929023+athirahwahhab@users.noreply.github.com>
2025-02-03 05:07:00 +0800

    Update ayam standard.py

--- pages/ayam standard.py
+++ pages/ayam standard.py
@@ -69,3 +69,10 @@ plt.tight_layout()
 plt.show()
 # Display the plot in Streamlit
 st.pyplot(plt.gcf())
+
+multi = '''If you end a line with two spaces,
+a soft return is used for the next line.
+
+Two (or more) newline characters in a row will result in a hard return.
+'''
+st.markdown(multi)
