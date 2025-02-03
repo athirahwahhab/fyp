19f6fd8471248fd7975fd7f7594515ef21d5a007
athirahwahhab <93929023+athirahwahhab@users.noreply.github.com>
2025-02-04 06:33:59 +0800

    Update sayur sawi.py

--- pages/sayur sawi.py
+++ pages/sayur sawi.py
@@ -154,7 +154,7 @@ with tab2:
   
   # Load and prepare the dataset
   try:
-      data = pd.read_csv('https://raw.githubusercontent.com/athirahwahhab/fyp/refs/heads/main/data/combined_output_latest.csv')
+      data = pd.read_csv('https://raw.githubusercontent.com/athirahwahhab/fyp/refs/heads/main/data/combined_filtered_allyears%20.csv')
   
       # Filter for item_code 1558 and process dates
       item_1558_data = data[data['item_code'] == 1558].copy()
@@ -268,7 +268,7 @@ with tab3:
   
   # Load and prepare data
   item_code = 1558  # Specify which item to analyze
-  df = pd.read_csv('https://raw.githubusercontent.com/athirahwahhab/fyp/refs/heads/main/data/combined_output_latest.csv', parse_dates=['date'])
+  df = pd.read_csv('https://raw.githubusercontent.com/athirahwahhab/fyp/refs/heads/main/data/combined_filtered_allyears%20.csv', parse_dates=['date'])
   item_data = df[df['item_code'] == item_code].copy()
   
   # Calculate daily average
