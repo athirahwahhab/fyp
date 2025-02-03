81212fd677fcf21e5c335a46d736279f55da1715
athirahwahhab <93929023+athirahwahhab@users.noreply.github.com>
2025-02-04 02:16:32 +0800

    Update telur gred C .py

--- pages/telur gred C .py
+++ pages/telur gred C .py
@@ -248,7 +248,7 @@ with tab3:
   
   # Load and prepare data
   item_code = 1111  # Specify which item to analyze
-  df = pd.read_csv('https://raw.githubusercontent.com/athirahwahhab/fyp/refs/heads/main/data/combined_output_latest.csv'', parse_dates=['date'])
+  df = pd.read_csv('https://raw.githubusercontent.com/athirahwahhab/fyp/refs/heads/main/data/combined_output_latest.csv', parse_dates=['date'])
   item_data = df[df['item_code'] == item_code].copy()
   
   # Calculate daily average
